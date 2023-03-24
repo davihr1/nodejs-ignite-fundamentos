80f6ecc3826b679acbdffe763e4ad638de4b4eca
davihr1 <davihenriquefavaro@gmail.com>
2023-03-24 00:32:25 -0300

    end_to_project_01

--- src/server.js
+++ src/server.js
@@ -1,23 +1,27 @@
 import http from 'node:http';
+import { json } from './middleware/json.js';
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+
+    await json(req, res)
+
     const {method, url} = req
 
     if (method === 'GET' && url === '/users') {
-        return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+        return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
 
+        const {name, email} = req.body;
+
         users.push({
             id: 1,
-            name: 'Davi Henrique',
-            email: 'davihenrique@email.com',
-        })
+            name,
+            email
+        });
 
         return res.writeHead(201).end()
     }
